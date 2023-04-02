54210e5b9cce2bec3137b12e6b8fcee255818094
Franarka <129119048+Franarka@users.noreply.github.com>
2023-04-02 21:29:05 +0200

    Add files via upload
    He incluido apuestas......

--- RetoDados.py
+++ RetoDados.py
@@ -5,6 +5,8 @@ def mis_dados():
     
     n=random.randint(1, 6)
     m=random.randint(1, 6)
+
+
     resp=(input("Quieres tirar los dados (s/n) : "  ))
     time.sleep(2)
     suma=n+m
@@ -12,21 +14,28 @@ def mis_dados():
     if resp == "s":
     
         print(n, m,"= ", suma)
-        break    
-    else:
+        
+    elif resp == "n":
+
         print("Adios")
-        quit()
-    
+        quit ()
+        
+
+        
+
+
+        
+      
     
     time.sleep(1) 
           
     if (suma == 7 or suma == 11):
-        print("Todos ganan")
+            print("Todos ganan")
     elif (suma ==2 or suma == 6 or suma == 12):
-        print("Jugador gana")
+            print("Jugador gana")
         
     else:
-        print("Siga probando")   
+            print("Siga probando")   
        
 
 mis_dados()
